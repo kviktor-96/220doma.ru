9beb785fb15095744634362a0f96e4373330c7a7
kviktor-96 <kviktor-96@mail.ru>
2021-11-01 17:19:58 +0300

    pluguin del

--- _plugins/jekyll-webp-master/lib/jekyll-webp.rb
+++
@@ -1,9 +0,0 @@
-require "jekyll-webp/version"
-require "jekyll-webp/defaults"
-require "jekyll-webp/webpExec"
-require "jekyll-webp/webpGenerator"
-
-module Jekyll 
-  module Webp
-  end # module Webp
-end # module Jekyll
